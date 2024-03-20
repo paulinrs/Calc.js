0ee735db59ce53e6190ac04f41a3d0d94cc2a55f
ruby --version <paulinn.rs@gmail.com>
2024-03-20 20:31:54 -0300

    themeSwitcher

--- index.js
+++ index.js
@@ -31,12 +31,25 @@ input.addEventListener("keydown", function (ev) {
 })
 
 document.getElementById("equal").addEventListener("click", calculate)
-document.getElementsById("equalmais").addEventListener("click", calculate)
 
 function calculate() {
-  resultInput.value = "ERROR"
-  resultInput.classList.add("error")
   const result = eval(input.value)
   resultInput.value = result
-  resultInput.classList.remove("error")
-}
+  }
+
+  
+  document.getElementById("themeSwitcher").addEventListener("click", function () {
+    if (main.dataset.theme === "dark") {
+      root.style.setProperty("--bg-color", "#f1f5f9")
+      root.style.setProperty("--border-color", "#aaa")
+      root.style.setProperty("--font-color", "#212529")
+      root.style.setProperty("--primary-color", "#666")
+      main.dataset.theme = "light"
+    } else {
+      root.style.setProperty("--bg-color", "#212529")
+      root.style.setProperty("--border-color", "#666")
+      root.style.setProperty("--font-color", "#f1f5f9")
+      root.style.setProperty("--primary-color", "#e80808")
+      main.dataset.theme = "dark"
+    }
+  })
