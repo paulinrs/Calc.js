133b3c5356dfd78cf9b58b70371d4f15f1003377
ruby --version <paulinn.rs@gmail.com>
2024-03-31 12:08:08 -0300

    copy

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ function calculate() {
   }
 
   
+  document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+    const button = ev.currentTarget
+    if (button.innerText === "Copiar") {
+      button.innerText = "Copiado!"
+      button.classList.add("success")
+      navigator.clipboard.writeText(resultInput.value)
+    } else {
+      button.innerText = 'Copiar'
+      button.classList.remove("success")
+    }
+  })    
+  
   document.getElementById("themeSwitcher").addEventListener("click", function () {
     if (main.dataset.theme === "dark") {
       root.style.setProperty("--bg-color", "#f1f5f9")
