c39af60af06bf1006f13c26cc1638e4fd98a9ec8
ruby --version <paulinn.rs@gmail.com>
2024-04-05 14:22:37 -0300

    bordRegions

--- jogo_da_velha/index.js
+++ jogo_da_velha/index.js
@@ -1,1 +1,8 @@
-const bordRegions = 
+const bordRegions = document.querySelectorAll('#gameBoard span')
+const vBoard = []
+let turnPlayer = ''
+
+function updateTitle() {
+    const playerInput = document.getElementById(turnPlayer)
+    document.getElementById ('turnPlayer').innerText = playerInput.value
+}
