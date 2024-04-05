6e6eb09b8c0ece1ea20ba5d22c5893d8e67c8c30
ruby --version <paulinn.rs@gmail.com>
2024-04-05 20:02:15 -0300

    function  initializeGame() {

--- jogo_da_velha/index.js
+++ jogo_da_velha/index.js
@@ -1,8 +1,20 @@
-const bordRegions = document.querySelectorAll('#gameBoard span')
+const boardRegions = document.querySelectorAll('#gameBoard span')
 const vBoard = []
 let turnPlayer = ''
 
 function updateTitle() {
     const playerInput = document.getElementById(turnPlayer)
     document.getElementById ('turnPlayer').innerText = playerInput.value
+}
+
+function  initializeGame() {
+    vBoard = [ ['','',''], ['','',''], ['','','']]
+    turnPlayer = 'player1'
+    document.querySelector('h2').innerHTML ='Vez de: <span id="turnPlayer"></span>'
+    updateTitle()
+    boardRegions.forEach(function (element){
+        element.classList.remove('win')
+        element.innerText = ''
+        element.addEventListener('click', handleBoardClick)
+    })
 }
